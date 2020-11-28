3e1453016ba0ce479da1e957eb267b4e0829d9a7
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-28 16:48:07 -0500

    Add helpful comments. Import sqlite3 and initialize database

--- api/employees.js
+++ api/employees.js
@@ -1,4 +1,10 @@
+// This file handles logic for all routes starting with /api/employees/
+
+// Import Express and create employees router
 const express = require('express');
 const employeesRouter = express.Router();
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+// Export employeesRouter
 module.exports = employeesRouter;
