683ad0b36e3c3e4da3990360a905648e1d9b4510
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-28 16:51:36 -0500

    Add /api/employees GET route

--- api/employees.js
+++ api/employees.js
@@ -6,5 +6,16 @@ const employeesRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+
+employeesRouter.get('/', (req, res, next) => {
+    db.all('SELECT * FROM Employee WHERE is_current_employee = 1', (err, employees) => {
+        if (err) {
+            next(err);
+        } else {
+            res.status(200).json({ employees: employees });
+        }
+    });
+});
+
 // Export employeesRouter
 module.exports = employeesRouter;
