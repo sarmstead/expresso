b9a36589ba8654aff8a5983dc63df0b57ca32d9f
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-28 16:11:35 -0500

    Import and use cors, body-parser, errorhandler, and morgan middleware functions

--- server.js
+++ server.js
@@ -2,6 +2,18 @@
 const express = require('express');
 const app = express();
 
+// Import additional middleware
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const errorhandler = require('errorhandler');
+const morgan = require('morgan');
+
+// Use additional middleware for every route
+app.use(cors());
+app.use(bodyParser.json());
+app.use(errorhandler());
+app.use(morgan('dev'));
+
 // Set PORT variable
 const PORT = process.env.PORT || 4000;
 
