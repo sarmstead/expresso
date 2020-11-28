79822abd3a4b7c3273dcb78c41ddee0f9ac40cd9
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-28 16:24:07 -0500

    Add useful comments. Import employeesRouter and mount it at /api/employees

--- api/api.js
+++ api/api.js
@@ -1,4 +1,12 @@
+// Initialize top-level API router '/api/*'
 const express = require('express');
 const apiRouter = express.Router();
 
+// Import additional routers to append to API router
+const employeesRouter = require('./employees');
+
+// Mount additional routers
+apiRouter.use('/employees', employeesRouter);
+
+// Export apiRouter
 module.exports = apiRouter;
