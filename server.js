1791dabe0c1d1823b61d7a74bce40414b2447dd7
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-28 16:15:20 -0500

    Import and use API router for all routes starting with /api

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 
+// Import API router
+const apiRouter = require('./api/api');
+
 // Import additional middleware
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -13,6 +16,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(errorhandler());
 app.use(morgan('dev'));
+
+// Mount api router for all routes starting with '/api'
+app.use('/api', apiRouter);
 
 // Set PORT variable
 const PORT = process.env.PORT || 4000;
