ff569e93943a1c6c9c90043f9b3e7870a6d3c84b
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-11-29 13:23:35 -0500

    Create param logic for routes ending with /:employeeId

--- api/employees.js
+++ api/employees.js
@@ -6,6 +6,21 @@ const employeesRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+// Param for /:employeeId
+employeesRouter.param('/:employeeId', (req, res, next, employeeId) => {
+    const sql = 'SELECT * FROM Employee WHERE Employee.id = $employeeId';
+    const values = { $employeeId: employeeId };
+    db.get(sql, values, (err, employee) => {
+        if (err) {
+            next(err);
+        } else if (employee) {
+            req.employee = employee;
+            next();
+        } else {
+            res.sendStatus(404);
+        }
+    });
+});
 
 employeesRouter.get('/', (req, res, next) => {
     db.all('SELECT * FROM Employee WHERE is_current_employee = 1', (err, employees) => {
