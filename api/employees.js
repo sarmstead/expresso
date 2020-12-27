890ca4e7adcd8335d73a895d0875413f0c77fb03
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-12-27 14:31:08 -0500

    Create api/employees/:employeeId PUT route

--- api/employees.js
+++ api/employees.js
@@ -76,5 +76,34 @@ employeesRouter.get('/:employeeId', (req, res, next) => {
     });
 });
 
+employeesRouter.put('/:employeeId', (req, res, next) => {
+    const name = req.body.employee.name;
+    const position = req.body.employee.position;
+    const wage = req.body.employee.wage;
+    if (!name || !position || !wage) {
+        res.sendStatus(400);
+    }
+
+    const isCurrentEmployee = req.body.employee.isCurrentEmployee === 0 ? 0 : 1;
+
+    const sql = 'UPDATE Employee SET name = $employeeName, position = $employeePosition, wage = $employeeWage, is_current_employee = $isCurrentEmployee WHERE Employee.id = $employeeId';
+    const values = {
+        $employeeId: req.params.employeeId,
+        $employeeName: name,
+        $employeePosition: position,
+        $employeeWage: wage,
+        $isCurrentEmployee: isCurrentEmployee
+    };
+    
+    db.run(sql, values, (err) => {
+        if (err) {
+            next(err);
+        }
+        db.get(`SELECT * FROM Employee WHERE Employee.Id = ${req.params.employeeId}`, (err2, employee) => {
+            res.status(200).json({employee: employee});
+        });
+    });
+});
+
 // Export employeesRouter
 module.exports = employeesRouter;
