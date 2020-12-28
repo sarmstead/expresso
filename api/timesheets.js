0962a3bc625a2c1340959be73cf75d2614038a32
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-12-27 19:41:30 -0500

    Add mergeParams to router to allow timehsheets to access params from employees.js

--- api/timesheets.js
+++ api/timesheets.js
@@ -2,7 +2,7 @@
 
 // Import Express and create timesheets router
 const express = require('express');
-const timesheetsRouter = express.Router();
+const timesheetsRouter = express.Router({ mergeParams: true });
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
