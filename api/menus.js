6cba82f7256e58ddb59206db98941de225b61fdb
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-02-27 15:57:05 -0500

    Update comment. Export menusRouter.

--- api/menus.js
+++ api/menus.js
@@ -1,7 +1,10 @@
-// This file handles logic for all routes starting with /api/employees/
+// This file handles logic for all routes starting with /api/menus/
 
 // Import Express and SQlite3, and create menus router
 const express = require('express');
 const menusRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+// Export menusRouter
+module.exports = menusRouter;
