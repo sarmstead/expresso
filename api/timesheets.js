1a39a24dc5943c40fc4c75a40b0fc00b9ccb314c
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-02-27 12:52:58 -0500

    Create /api/employees/:employeeId/timesheets POST route

--- api/timesheets.js
+++ api/timesheets.js
@@ -21,3 +21,47 @@ timesheetsRouter.get('/', (req, res, next) => {
         }
     });
 });
+
+timesheetsRouter.post('/', (req, res, next) => {
+     const hours = req.body.timesheet.hours;
+     const rate = req.body.timesheet.rate;
+     const date = req.body.timesheet.date;
+     const employeeId = req.body.timesheet.employeeId;
+
+     const employeeSql = 'SELECT * FROM Employee WHERE Employee.id = $employeeId';
+     const employeeValues = { $employeeId: employeeId };
+    
+     // Determine if employee exists
+     db.get(employeeSql, employeeValues, (err, employee) => {
+         // Handle error if there is an issue querying database
+         if (err) {
+             next(err);
+         } else {
+             // Throw 400 error code if required data is not sent in req.body.timesheet
+             if (!hours || !rate || !date || !employeeId) {
+                 return res.sendStatus(400);
+             }
+             const timesheetSql = 'INSERT INTO Timesheet (id, hours, rate, date, employee_id) VALUES ($id, $hours, $rate, $date, $employeeId)';
+             const timesheetValues = {
+                $id: req.params.timesheetId, 
+                $hours: hours, 
+                $rate: rate, 
+                $date: date, 
+                $employeeId: employeeId
+             };
+             // If there are no errors querying specified employee, insert their timesheet into database
+             db.run(timesheetSql, timesheetValues, function(err2) {
+                 if (err2) {
+                     next(err2);
+                 }
+                 // If there are no errors inserting timesheet into database, fetch the newly created timesheet to return as a JSON object
+                 db.get(`SELECT * FROM Timesheet WHERE Timesheet.id = ${this.lastID}`, (err3, timesheet) => {
+                     if (err3) {
+                         next(err3);
+                     }
+                     res.status(201).json({ timesheet: timesheet });
+                 });
+             });
+         }
+     });
+});
