4324f1a3aaf00c4a4934252c02551be5a88748c7
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-02-27 17:39:37 -0500

    Create /api/menus/:menuId/menu-items/ GET route

--- api/menu-items.js
+++ api/menu-items.js
@@ -6,5 +6,14 @@ const menuItemsRouter = express.Router({ mergeParams: true });
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+menuItemsRouter.get('/', (req, res, next) => {
+    db.all(`SELECT * FROM MenuItem WHERE MenuItem.menu_id = ${req.params.menuId}`, (err, menuItems) => {
+        if (err) {
+            next(err);
+        }
+        res.status(200).json({menuItems: menuItems});
+    });
+});
+
 // Export menuItemsRouter
 module.exports = menuItemsRouter;
